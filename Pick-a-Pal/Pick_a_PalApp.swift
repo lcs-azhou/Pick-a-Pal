5c7006e8a980663edf86cb801e99454553ef8331
Ansheng Zhou <azhou@lcs.on.ca>
2024-01-16 14:29:06 -0500

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Ansheng Zhou on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
